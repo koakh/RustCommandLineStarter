5f9778e1f073407606da21fa5fdb1bb89a17e48c
Mario Monteiro <marioammonteiro@gmail.com>
2021-03-15 11:54:48 +0000

    using structopt

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
+use structopt::StructOpt;
+
 // in Rust, it is common to structure programs around the data they handle
 #[derive(Debug)]
+#[derive(StructOpt)]
 pub struct Cli {
+  /// the pattern to look for
   pub pattern: String,
+  /// the path to the file to read
+  #[structopt(parse(from_os_str))]
   pub path: std::path::PathBuf,
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,8 @@
-#![allow(unused_imports)]
-use command_line_apps_in_rust::{greeting, Cli};
+// #![allow(unused_imports)]
+use command_line_apps_in_rust::{Cli};
+use structopt::StructOpt;
 
 fn main() {
-  // no used variable, prefixed with _
-  let pattern = std::env::args().nth(1).expect("no pattern given");
-  let path = std::env::args().nth(2).expect("no path given");
-  // same with match option
-  // let path_option = std::env::args().nth(2);
-  // let path = match path_option {
-  //   Some(v) => v,
-  //   None => panic!("no path"),
-  // };
-  let args = Cli {
-    pattern,
-    path: std::path::PathBuf::from(path),
-  };
+  let args = Cli::from_args();
   println!("{:?}", args);
 }
